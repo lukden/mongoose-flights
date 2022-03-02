441bdef0d33c8430ae44fc3fd93725fee37a5618
lukden <dean.l@northeastern.edu>
2022-03-01 20:44:57 -0500

    add a flight

--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 const router = Router()
+import * as flightsCtrl from '../controllers/flight.js'
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
+router.get('/new', flightsCtrl.new)
+
+router.post('/', flightsCtrl.create)
+
 
 export {
   router
